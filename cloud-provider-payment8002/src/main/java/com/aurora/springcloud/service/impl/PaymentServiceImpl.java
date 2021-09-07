d5166479de452fc87598523909f14c525567a8bd
aurora <zhaoyanjun_3@163.com>
2021-09-07 14:53:19 +0800

    Initial commit

---
+++ cloud-provider-payment8002/src/main/java/com/aurora/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.aurora.springcloud.service.impl;
+
+import com.aurora.springcloud.dao.PaymentDao;
+import com.aurora.springcloud.entities.Payment;
+import com.aurora.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
